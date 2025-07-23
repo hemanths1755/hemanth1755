1670a2af92a8442f874dfa046975973dc11685c5
21Flash <thejasmr71@gmail.com>
2025-07-23 13:46:26 +0530

    h1

---
+++ hemanth.py
@@ -0,0 +1,9 @@
+print("Gulab Jamun, "
+      "cake,"
+      " Tiramisu,"
+      " Ice cream,"
+      " Apple pie,"
+      " Chocolate brownie,"
+      " Carrot cake,"
+      " Cookies, "
+      "Crème")
